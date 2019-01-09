c2442052ba54c6565dcf7122662965136843bb07
Michael Kelly <mike.s.kelly@icloud.com>
2019-01-09 09:08:51 -0800

    Set up MainMenu()

--- Lab02UnitTesting/Program.cs
+++ Lab02UnitTesting/Program.cs
@@ -6,7 +6,33 @@ namespace Lab02UnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainMenu();
+        }
+
+        static string MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1) Withdraw Funds");
+            Console.WriteLine("2) Deposit Funds");
+            Console.WriteLine("3) Check Balance");
+            Console.WriteLine("4) Exit");
+            string choice = Console.ReadLine();
+            MenuNav(choice);
+            return choice;
+        }
+        static void MenuNav(string choice)
+        {
+            if (choice == "1")
+            {
+                Withdraw();
+            } else if (choice == "2")
+            {
+                Deposit();
+            }
+        }
+        static void Withdraw()
+        {
+
         }
     }
 }
